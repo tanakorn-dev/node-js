e42df45082a13b6140fba80da2124d0af96801e3
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2020-03-24 02:43:34 +0700

    :sparkles: Seccion 5 end.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const http = require('http')
-const routes = require('./routes')
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser')
 
-console.log(routes.someText)
-const server = http.createServer(routes.handler)
+const app = express();
 
-server.listen(3000)
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
