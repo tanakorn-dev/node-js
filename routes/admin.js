8efdfb921f0f6eba7fb05f7eb7f47144f87e0137
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2020-04-08 17:01:21 +0700

    :sparkles: Section 7

--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-const products =[];
+// /admin/add-product => GET
+router.get('/add-product', productController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Ass Product', path: '/admin/add-product'});
-});
-
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-});
+// /admin/add-product => POST
+router.post('/add-product', productController.postAddProduct);
 
 exports.routes = router;
-exports.products = products;
